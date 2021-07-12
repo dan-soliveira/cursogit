10c793c96aab0fada2764a4660635fd6447b4603
Danilo Silva Oliveira <solivira.dan@gmail.com>
2021-07-12 14:20:22 -0300

    Projeto iniciado, mostrando a mensagem Salvee

---
+++ 98. Repositório Git/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _98._Repositório_Git {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Salvee");
+            Console.ReadLine();
+        }
+    }
+}
